cf8dd1c366ec3ef71f51ac74cddfd3991c90e038
orchardcc <45976390+orchardcc@users.noreply.github.com>
2020-04-17 23:55:52 -0400

    minor updates to echo messages

--- .github/actions/build-and-deploy/entrypoint.sh
+++ .github/actions/build-and-deploy/entrypoint.sh
@@ -8,17 +8,20 @@ echo "Installing dependencies required by site..."
 # Install Jekyll site dependencies as defined by the Gemfile
 bundle install
 
-echo "...done. Building site using Jekyll..."
+echo "Dependencies installed. Building site using Jekyll..."
 
 # Defines the directory where Jekyll shall write
 # the files it generates when it builds the site
 JEKYLL_SITE_DEST="./_site"
 
 # Build the Jekyll site
+# If you use the jekyll-github-metadata plugin, you must
+# set JEKYLL_GITHUB_TOKEN for it to get your github data
+# @see: https://github.com/jekyll/github-metadata/blob/master/docs/authentication.md
 JEKYLL_ENV=production JEKYLL_GITHUB_TOKEN=${GITHUB_TOKEN} \
 bundle exec jekyll build --destination ${JEKYLL_SITE_DEST}
 
-echo "...done. Priming site for deployment..."
+echo "Site built. Priming site for deployment..."
 
 # Go to the directory where the Jekyll-generated site resides
 cd ${JEKYLL_SITE_DEST}
@@ -36,7 +39,7 @@ rm -f README.md
 REMOTE_REPO="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 REMOTE_BRANCH="gh-pages"
 
-echo "...done. Deploying site to ${REMOTE_BRANCH} branch of ${GITHUB_REPOSITORY} repository..."
+echo "Site primed. Deploying site to ${REMOTE_BRANCH} branch of ${GITHUB_REPOSITORY} repository..."
 
 # Initialize a new local git repository
 git init
@@ -58,10 +61,10 @@ git push --force ${REMOTE_REPO} master:${REMOTE_BRANCH}
 
 # Presumably, these commands are unnecessary. Presumably, the virtual machine
 # used to run the GitHub Actions workflow is destroyed once the jobs are done
-echo "...done. Removing files generated whilst building and deploying site..."
+echo "Site deployed. Removing files generated whilst building and deploying site..."
 rm -rf .git
 cd ..
 bundle exec jekyll clean
 
-echo "...done. Site built and deployed. Huzzah!"
+echo "Files removed. All done. Huzzah!"
 exit 0
